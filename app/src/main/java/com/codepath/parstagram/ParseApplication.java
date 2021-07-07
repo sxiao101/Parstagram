0702279e46d3446b93bdc908931d450b2c8711c8
sxiao101 <sxiao1@fb.com>
2021-07-07 18:59:57 -0400

    Posting Photos (defining data model, image capture, & post creation)

--- app/src/main/java/com/codepath/parstagram/ParseApplication.java
+++ app/src/main/java/com/codepath/parstagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.codepath.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("G9krRttd0jGPfjDuIh3LvujfDr26kfenNmQ8lswq")
                 .clientKey("jh1qOzE28AO4tAPsNQzVjO6AHnmztL3kUfMlpWxI")
