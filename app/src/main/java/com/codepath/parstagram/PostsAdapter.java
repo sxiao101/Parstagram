f2d443fdea585f9b1c1365fae93bf71e36d9d72c
sxiao101 <sxiao1@fb.com>
2021-07-09 10:25:51 -0400

    details fragment

--- app/src/main/java/com/codepath/parstagram/PostsAdapter.java
+++ app/src/main/java/com/codepath/parstagram/PostsAdapter.java
@@ -3,6 +3,8 @@ package com.codepath.parstagram;
 import android.content.Context;
 import android.os.Bundle;
 import android.telecom.Call;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,7 @@ import com.bumptech.glide.Glide;
 import com.codepath.parstagram.fragments.DetailsFragment;
 import com.parse.ParseFile;
 
+import java.util.Date;
 import java.util.List;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
@@ -57,12 +60,14 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
         private TextView tvUsername;
         private ImageView ivImage;
         private TextView tvDescription;
+        private TextView tvTime;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvUsername = itemView.findViewById(R.id.tvUsername);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            tvTime = itemView.findViewById(R.id.tvTime);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -72,7 +77,13 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
                     Post post = posts.get(position);
                     bundle.putString("user", post.getUser().getUsername());
                     bundle.putString("description", post.getDescription());
-                    //bundle.putString("image", post.getImage());
+                    if (post.getImage() != null) {
+                        bundle.putBoolean("hasImage", true);
+                        bundle.putString("image", post.getImage().getUrl());
+                    } else {
+                        bundle.putBoolean("hasImage", false);
+                    }
+                    bundle.putString("time", calculateTimeAgo(post.getCreatedAt()));
                     DetailsFragment details = new DetailsFragment();
                     details.setArguments(bundle);
 
@@ -83,8 +94,10 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
         }
 
         public void bind(Post post) {
-            tvDescription.setText(post.getDescription());
             tvUsername.setText(post.getUser().getUsername());
+            String sourceString = "<b>" + post.getUser().getUsername() + "</b> " + post.getDescription();
+            tvDescription.setText(Html.fromHtml(sourceString));
+            tvTime.setText(calculateTimeAgo(post.getCreatedAt()));
             ParseFile image = post.getImage();
             if (image != null) {
                 ivImage.setVisibility(View.VISIBLE);
@@ -95,5 +108,41 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
                 ivImage.setVisibility(View.GONE);
             }
         }
+    }
+
+    public static String calculateTimeAgo(Date createdAt) {
+
+        int SECOND_MILLIS = 1000;
+        int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+        try {
+            createdAt.getTime();
+            long time = createdAt.getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
+        } catch (Exception e) {
+            Log.i("Error:", "getRelativeTimeAgo failed", e);
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
--- app/src/main/java/com/codepath/parstagram/fragments/DetailsFragment.java
+++ app/src/main/java/com/codepath/parstagram/fragments/DetailsFragment.java
@@ -2,65 +2,64 @@ package com.codepath.parstagram.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.parstagram.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView tvUsername;
+    private ImageView ivImage;
+    private TextView tvDescription;
+    private TextView tvTime;
 
     public DetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailsFragment newInstance(String param1, String param2) {
-        DetailsFragment fragment = new DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvUsername = view.findViewById(R.id.tvUsername);
+        ivImage = view.findViewById(R.id.ivImage);
+        tvDescription = view.findViewById(R.id.tvDescription);
+        tvTime = view.findViewById(R.id.tvTime);
+
+        Bundle bundle = this.getArguments();
+        String user = bundle.getString("user");
+        String description = bundle.getString("description");
+        tvUsername.setText(user);
+        String sourceString = "<b>" + user + "</b> " + description;
+        tvDescription.setText(Html.fromHtml(sourceString));
+
+        if(bundle.getBoolean("hasImage")) {
+            ivImage.setVisibility(View.VISIBLE);
+            Glide.with(getContext())
+                    .load(bundle.getString("image"))
+                    .into(ivImage);
+        } else {
+            ivImage.setVisibility(View.GONE);
+        }
+
+        tvTime.setText(bundle.getString("time"));
     }
 }
